cc38c00204f5775620aec3f0c251f131673745c7
Sebastiano Demichelis <sebastiano.demichelis@edu.itspiemonte.it>
2022-11-17 17:15:53 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Reset from "./pages/Reset/Reset";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
